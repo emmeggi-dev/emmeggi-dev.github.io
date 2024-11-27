0e10334622818ecb90d5b3c3d43d4fecf3966009
emmeggi-dev <145357057+emmeggi-dev@users.noreply.github.com>
2024-11-27 11:10:01 +0100

    Add files via upload

---
+++ webpages/anagrafica-2/index.js
@@ -0,0 +1,54 @@
+var name_ = document.getElementById("name");
+var surname = document.getElementById("surname");
+var address = document.getElementById("address");
+var city = document.getElementById("city");
+var email = document.getElementById("email");
+var cf = document.getElementById("cf");
+var table = document.getElementById("cool-table");
+
+var values = []
+
+function ok() {
+    if (
+        name_.value == "" ||
+        surname.value == "" ||
+        address.value == "" ||
+        city.value == "" ||
+        email.value == "" ||
+        cf.value == ""
+    ) return;
+
+    if (
+        !name_.checkValidity() ||
+        !surname.checkValidity() ||
+        !address.checkValidity() ||
+        !city.checkValidity() ||
+        !email.checkValidity() ||
+        !cf.checkValidity()
+    ) return;
+
+    values.push([
+        name_.value,
+        surname.value,
+        address.value,
+        city.value,
+        email.value,
+        cf.value
+    ]);
+
+    name_.value = "";
+    surname.value = "";
+    address.value = "";
+    city.value = "";
+    email.value = "";
+    cf.value = "";
+
+    drawTable();
+}
+
+function drawTable() {
+    var html = "<tr><th>Nome</th><th>Cognome</th><th>Indirizzo</th><th>Città</th><th>Email</th><th>Codice Fiscale</th></tr>"
+    for (var i of values)
+        html += `<tr><td>${i[0]}</td><td>${i[1]}</td><td>${i[2]}</td><td>${i[3]}</td><td>${i[4]}</td><td>${i[5]}</td></tr>`
+    table.innerHTML = html;
+}
