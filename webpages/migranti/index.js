affb0f599468bd131054073615ebc73b79510412
emmeggi-dev <145357057+emmeggi-dev@users.noreply.github.com>
2024-12-18 11:04:35 +0100

    Add files via upload

--- webpages/migranti/index.js
+++ webpages/migranti/index.js
@@ -9,9 +9,19 @@ function apri(input){
         let heading = true;
         let tagEnd = "th>";
         let tableContent = "";
+
+        const xValues = [];
+        const yValues = [];
+
         for (let row of reader.result.split("\n")) {
             tableContent += "<tr>";
-            for (let cell of row.split(","))
+            let splitRow = row.split(",");
+            let xValue = Number(splitRow[0].substring(1,splitRow[0].length-1));
+            if (!isNaN(xValue)) {
+                xValues.push(Number(splitRow[0].substring(1,splitRow[0].length-1)));
+                yValues.push(Number(splitRow[1].substring(1,splitRow[1].length-1)));
+            }
+            for (let cell of splitRow)
                 tableContent += `<${tagEnd}${cell.substring(1,cell.length-1)}</${tagEnd}`;
             if (heading) {
                 tagEnd = "td>";
@@ -20,5 +30,19 @@ function apri(input){
             tableContent += "</tr>";
         }
         table.innerHTML = tableContent;
-    };
-};
+
+        new Chart("grafico", {
+            type: "bar",
+            data: {
+              labels: xValues,
+              datasets: [{
+                backgroundColor: "blue",
+                data: yValues
+              }]
+            },
+            options: {
+              legend: {display: false}
+            }
+          });
+    }
+}
